66448713d66d10f92135ba0ea31740bfe8a23506
zhibirc <zhibirc@users.noreply.github.com>
2014-07-08 20:23:50 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 (function() {
+    amihtml5Init();
     if (window.addEventListener) window.addEventListener('load', outputDetection, false);
     else if (window.attachEvent) window.attachEvent('onload', outputDetection);
     else window.onload = outputDetection;
@@ -6,133 +7,18 @@
 
 function outputDetection() {
     with (document) {
-        getElementById('canvas').innerHTML = featuresHTML5detect().canvas();
-        getElementById('canvas_text').innerHTML = featuresHTML5detect().canvasText();
-        getElementById('video').innerHTML = featuresHTML5detect().video();
-        getElementById('video_type').innerHTML = featuresHTML5detect().videoTypes();
-        getElementById('geo').innerHTML = featuresHTML5detect().geo();
-        getElementById('worker').innerHTML = featuresHTML5detect().webWorker();
-        getElementById('off_app').innerHTML = featuresHTML5detect().offApp();
-        getElementById('storage').innerHTML = featuresHTML5detect().lStorage();
-        getElementById('i_type').innerHTML = featuresHTML5detect().iType();
-        getElementById('history').innerHTML = featuresHTML5detect().historyAPI();
-        getElementById('autofocus').innerHTML = featuresHTML5detect().autoFocus();
-        getElementById('placeholder').innerHTML = featuresHTML5detect().placeHolder();
-        getElementById('microdata').innerHTML = featuresHTML5detect().microData();
+        getElementById('canvas').innerHTML = amihtml5.canvas;
+        getElementById('canvas_text').innerHTML = amihtml5.canvas_text;
+        getElementById('video').innerHTML = amihtml5.video;
+        getElementById('geo').innerHTML = amihtml5.geo;
+        getElementById('worker').innerHTML = amihtml5.worker;
+        getElementById('off_app').innerHTML = amihtml5.off_app;
+        getElementById('storage').innerHTML = amihtml5.storage;
+        // or any other type from the 13 of available
+        getElementById('i_type').innerHTML = amihtml5.input.search;
+        getElementById('history').innerHTML = amihtml5.history;
+        getElementById('autofocus').innerHTML = amihtml5.autofocus;
+        getElementById('placeholder').innerHTML = amihtml5.placeholder;
+        getElementById('microdata').innerHTML = amihtml5.microdata;
     }
-}
-function featuresHTML5detect() {
-    return {
-        canvas: function() {
-            return !!document.createElement('canvas').getContext;
-        },
-        canvasText: function() {
-            return this.canvas() ? typeof document.createElement('canvas').getContext('2d').fillText == 'function' : 'false';
-        },
-        video: function() {
-            return !!document.createElement('video').canPlayType;
-        },
-        videoTypes: function() {
-            var elem = document.createElement('video'),
-                support = [];
-            if (this.video()) {
-                if (elem.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"') == 'probably') support.push('H.264, AAC, MP4');
-                if (elem.canPlayType('video/ogg; codecs="theora, vorbis"') == 'probably') support.push('Ogg Vorbis');
-                if (elem.canPlayType('video/webm; codecs="vp8, vorbis"') == 'probably') support.push('WebM');
-                return support ? support.join('; ') : 'Other';
-            } else {
-                return 'Not supported any formats';
-            }
-        },
-        geo: function() {
-            return !!navigator.geolocation;
-        },
-        webWorker: function() {
-            return !!window.Worker;
-        },
-        offApp: function() {
-            return !!window.applicationCache;
-        },
-        lStorage: function() {
-            try {
-                return 'localStorage' in window && window['localStorage'] !== null;
-            } catch(e) {
-                return false;
-            }
-        },
-        iType: function() {
-            var elem = document.createElement('input'),
-                types = [
-                    {
-                        type: 'search',
-                        support: ''
-                    },
-                    {
-                        type: 'number',
-                        support: ''
-                    },
-                    {
-                        type: 'range',
-                        support: ''
-                    },
-                    {
-                        type: 'color',
-                        support: ''
-                    },
-                    {
-                        type: 'tel',
-                        support: ''
-                    },
-                    {
-                        type: 'url',
-                        support: ''
-                    },
-                    {
-                        type: 'email',
-                        support: ''
-                    },
-                    {
-                        type: 'date',
-                        support: ''
-                    },
-                    {
-                        type: 'month',
-                        support: ''
-                    },
-                    {
-                        type: 'week',
-                        support: ''
-                    },
-                    {
-                        type: 'time',
-                        support: ''
-                    },
-                    {
-                        type: 'datetime',
-                        support: ''
-                    },
-                    {
-                        type: 'datetime-local',
-                        support: ''
-                    }
-                ], results = [];
-            for (var i = 0; i < types.length; i++) {
-                types[i].support = elem.setAttribute('type', types[i].type) !== 'text';
-                results.push(types[i].type + ': ' + types[i].support);
-            }
-            return results.join('; ');
-        },
-        historyAPI: function() {
-            return !!(window.history && history.pushState);
-        },
-        autoFocus: function() {
-            return 'autofocus' in document.createElement('input');
-        },
-        placeHolder: function() {
-            return  'placeholder' in document.createElement('input');
-        },
-        microData: function() {
-            return !!document.getItems;
-        }
-    };
 }
